f15afa7efbc49ab66084ec2d9d5d3a5494ea01a6
Kelly Plyler <kellysplyler@gmail.com>
2023-06-30 09:42:25 -0500

    completed variables exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace VariablesExercise
+﻿using System.Net;
+
+namespace VariablesExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string destination = "Cancun";
+            int numberOfGuests = 2;
+            char currency = '$';
+            bool isSummer = true;
+            double costOfDrink = 5.50;
+            decimal costOfMeal = 15.95m;
+
+            Console.WriteLine($"This Summer I'm going to {destination} with my husband, just the {numberOfGuests} of us. The whole trip cost {currency}500 and that is {isSummer}! One margarita costs ${costOfDrink} and one burrito costs ${costOfMeal}.");
         }
     }
 }
